2eb1836a03e251427d57b7081c8790d121308f4e
Nikita Bolshakov <bnikmik@gmail.com>
2022-12-13 12:24:30 +0300

    Домашнее задание 1.8: Методы объектов

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,11 @@
 public class Main {
     public static void main(String[] args) {
+        Author author1 = new Author("Джоан", "Роулинг");
+        Author author2 = new Author("Фёдор", "Достоевский");
+        Book book1 = new Book("Гарри Поттер и философский камень", author1, 1997);
+        Book book2 = new Book("Преступление и наказание", author2, 1866);
+        System.out.println(book1);
+        book2.setPublicationYear(2020);
+        System.out.println(book2);
     }
 }
